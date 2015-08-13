2a7df0c4fa2caf713f7b8c383665d2e302a2412d
Matthew K. MacLeod <matthewmacleod28@gmail.com>
2015-08-13 15:32:06 -0600

    add cursor to class level

--- post_zeptat.py
+++ post_zeptat.py
@@ -3,15 +3,22 @@ import sys
 
 class Zeptat(object):
     con = None
+    cur = None
     db_name = 'zeptat'
 
     def version(self):
+        """  Should return version information, eg:
+
+          ('PostgreSQL 9.4.4 on x86_64-redhat-linux-gnu,
+          compiled by gcc (GCC) 5.1.1 20150422 (Red Hat 5.1.1-1), 64-bit',)
+
+          Note: database (db_name) must exist previous to running
+        """
         try:
-            # note that datbase must exist previous to running
             self.con = psycopg2.connect(database=self.db_name, user='postgres')
-            cur = self.con.cursor()
-            cur.execute('SELECT version()')
-            ver = cur.fetchone()
+            self.cur = self.con.cursor()
+            self.cur.execute('SELECT version()')
+            ver = self.cur.fetchone()
             print ver
 
         except psycopg2.DatabaseError, e:
@@ -20,6 +27,7 @@ class Zeptat(object):
 
 
     def close(self):
+        self.cur.close()
         self.con.close()
 
 
