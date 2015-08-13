141f1cded53a4367c85899207c9df9c723819530
Matthew K. MacLeod <matthewmacleod28@gmail.com>
2015-08-13 16:11:22 -0600

    add postgres table creation

--- post_zeptat.py
+++ post_zeptat.py
@@ -2,9 +2,11 @@ import psycopg2
 import sys
 
 class Zeptat(object):
+    """ Simple class to interact with PostgreSQL server """
     con = None
     cur = None
     db_name = 'zeptat'
+    table_name = 'ebooks'
 
     def version(self):
         """  Should return version information, eg:
@@ -22,8 +24,31 @@ class Zeptat(object):
             print ver
 
         except psycopg2.DatabaseError, e:
-            print 'Error %s' % e
+            print 'Version error %s' % e
             sys.exit(1)
+
+    def create_table(self):
+        try:
+            """ create zeptat postgres schema, note will drop table if exists """
+            print "Creating table..."
+            sql = "DROP TABLE IF EXISTS {0}".format(self.table_name)
+            self.cur.execute(sql)
+            sql_table = "CREATE TABLE {0} (title TEXT PRIMARY KEY, line INT, line_content TEXT);".format(self.table_name)
+            self.cur.execute(sql_table)
+            self.con.commit()
+
+        except psycopg2.DatabaseError, e:
+            print 'Create table error %s' % e
+
+    def load_data(self, ebook_data):
+        try:
+            inserts = "INSERT INTO {0} (title,line,line_content) Values (%s, %s, %s)".format(self.table_name)
+            self.cur.executemany(insters, ebook_data)
+            self.con.commit()
+
+        except psycopg2.DatabaseError, e:
+            print 'Load data error %s' % e
+
 
 
     def close(self):
@@ -37,5 +62,11 @@ class Zeptat(object):
 if __name__ == '__main__':
   zeptat = Zeptat()
   zeptat.version()
+
+  if sys.argv[1] == 'upload':
+      zeptat.create_table()
+
+
+
   zeptat.close()
 
